a491a2d2fe7ca65979c24dfcb450224696136a78
Jonathan Holvey <jonathan.holvey@outlook.com>
2017-08-04 07:15:47 +1000

    Add basic API to handle simple authentication

--- index.php
+++ index.php
@@ -1,17 +1,41 @@
 <?php
+	include_once("strings.php");
+
 	// Load URL database
 	$database = json_decode(file_get_contents("database.json"), true);
+	$action = $_GET["action"];
 
-	// Check forwarding ID
-	if (isset($_GET["id"]) and array_key_exists($_GET["id"], $database))
-		$id = $_GET["id"];
-	else
-		$id = "_default";
+	if ($action == "api") {
+		// Retrieve JSON from POST data
+		$data = json_decode(file_get_contents("php://input"), true);
+		if ($data == null)
+			$data = array();
 
-	// Find destination URL
-	$url = $database[$id];
+		$response = array();
 
-	// Forward browser
-	header("Location: " . $url, true, 301);
-	exit();
+		// Authenticate by checking provided secret
+		if (!array_key_exists("secret", $data) or $data["secret"] != file_get_contents("secret")) {
+			$response["status"] = RESPONSE_ERROR;
+			$response["message"] = MESSAGE_BAD_AUTH;
+			$response["status-code"] = STATUS_UNAUTHORIZED;
+		}
+		else
+			$response["status"] = RESPONSE_SUCCESS;
+			$response["status-code"] = STATUS_OK;
+
+		// Return response as JSON
+		echo json_encode($response);
+	}
+
+	else if ($action == "forward") {
+		// Check forwarding ID
+		if (isset($_GET["id"]) and array_key_exists($_GET["id"], $database))
+			$id = $_GET["id"];
+		else
+			$id = "_default";
+
+		// Forward browser
+		header("Location: " . $database[$id], true, 301);
+		exit();
+	}
 ?>
---
+++ strings.php
@@ -0,0 +1,10 @@
+<?php
+	define(RESPONSE_ERROR, "error");
+	define(RESPONSE_SUCCESS, "success");
+
+	define(MESSAGE_BAD_AUTH, "Authentication failed. The secret you provided could not be verified.");
+
+	define(STATUS_OK, 200);
+	define(STATUS_CREATED, 201);
+	define(STATUS_UNAUTHORIZED, 401);
+?>
