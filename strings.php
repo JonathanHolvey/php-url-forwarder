8af1d39335b9d0ca1a5c9c90c813c28695f4ff3b
Jonathan Holvey <jonathan.holvey@outlook.com>
2018-11-27 19:18:24 +1000

    Fix typo in message string

--- strings.php
+++ strings.php
@@ -4,7 +4,7 @@
 
 	define(MESSAGE_BAD_AUTH, "Authentication failed. The secret you provided could not be verified.");
 	define(MESSAGE_ALREADY_EXISTS, "The ID provided already exists and cannot be replaced using the 'add' method. Use 'publish' instead.");
-	define(MESSAGE_ALREADY_DELETED, "The ID provided did not exist in the database.");
+	define(MESSAGE_ALREADY_DELETED, "The ID provided does not exist in the database.");
 
 	define(STATUS_OK, 200);
 	define(STATUS_CREATED, 201);
